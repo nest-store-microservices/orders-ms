685df7a3a26a4c8030a23cabfd946ca5c6e62b71
cpandares <pandaresocesar@gmail.com>
2025-04-23 12:21:39 -0400

    feat: set up Prisma with PostgreSQL, add order model and service, and update documentation

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
 </p>
 
+## Clonar repositorio
+
+## Instalar dependencias
+
+```bash
+npm install
+```
+
+## Clonar .env.example
+
+```bash
+cp .env.example .env
+```
+
+## Levantar base de datos con docker
+
+```bash
+docker-compose up -d
+```
+
+## Levantar el proyecto
+
+```bash
+npm run start:dev
+```
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 
+import { PrismaClient } from '@prisma/client';
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrdersService');
+
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
@@ -16,7 +24,7 @@ export class OrdersService {
     return `This action returns a #${id} order`;
   }
 
-  changeOrderStatus(){
+  changeOrderStatus() {
     return `This action changes the order status`;
   }
 }
